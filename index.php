db9cbab07ff624a4e5e8c29bc1c6ee68aa5aa83e
Shubham <shubham@itsbhm.group>
2021-02-08 12:57:38 +0530

    Root index.php file modified

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
 <?php
 
-$x = 10;
-$y = 2;
-$sum = $x + $y;
-$sub = $x - $y;
-$mul = $x * $y;
-$div = $x / $y;
-
-$msg_for_sum = "The sum of $x and $y is: $sum";
-//
-//
-//
-
-
-echo $msg_for_sum;
