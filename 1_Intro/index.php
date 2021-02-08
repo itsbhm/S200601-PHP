c0642091973adaf468890024d93ee71a52a096d4
Shubham <shubham@itsbhm.group>
2021-02-08 12:55:40 +0530

    File About Introduction, Installation, Syntax, Comments, Variables

---
+++ 1_Intro/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// 1_Intro
+
+$x = 10;
+$y = 2;
+$sum = $x + $y;
+$sub = $x - $y;
+$mul = $x * $y;
+$div = $x / $y;
+
+$msg_for_sum = "The sum of $x and $y is: $sum";
+$msg_for_sub = "The sub of $x and $y is: $sub";
+$msg_for_mul = "The mul of $x and $y is: $mul";
+$msg_for_div = "The div of $x and $y is: $div";
+
+echo $msg_for_sum;
+echo '<hr>';
+
+echo $msg_for_sub;
+echo '<hr>';
+
+echo $msg_for_mul;
+echo '<hr>';
+
+echo $msg_for_div;
