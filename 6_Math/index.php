f1cdcd518721c897ce93b0e535fce0fc74ec2c60
Shubham <shubham@itsbhm.group>
2021-05-18 12:24:07 +0530

    Topic Math Added

---
+++ 6_Math/index.php
@@ -0,0 +1,53 @@
+<?php
+
+// PHP pi() Function
+
+echo(pi()); // returns 3.1415926535898
+
+// ---------------------
+
+// PHP min() and max() Functions
+
+$num = array(0, 150, 30, 20, -8, -200);
+
+echo (min($num));
+
+echo ('<hr>');
+
+echo (max($num));
+
+echo ('<hr>');
+// ---------------------
+
+// PHP abs() Function - absolute (positive)
+
+echo(abs(-6.7));  // returns 6.7
+
+echo ('<hr>');
+// ---------------------
+
+// PHP sqrt() Function
+
+echo(sqrt(64));  // returns 8
+
+echo ('<hr>');
+//----------------------
+
+// PHP round() Function
+
+echo(round(0.60));  // returns 1
+echo(round(0.49));  // returns 0
+
+echo ('<hr>');
+//----------------------
+
+//Random Numbers
+
+// echo(rand());
+echo(rand(10, 100));
+
+
+echo ('<hr>');
+//----------------------
+
+
