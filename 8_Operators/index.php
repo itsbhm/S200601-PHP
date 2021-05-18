04279e134721c2a68bf8dfa9ac32127e6fd8b759
Shubham <shubham@itsbhm.group>
2021-05-18 12:45:03 +0530

    Topic Operators Added

---
+++ 8_Operators/index.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+
+PHP divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Increment/Decrement operators
+Logical operators
+String operators
+Array operators
+Conditional assignment operators
+
+-----------------------------
+
+PHP Arithmetic Operators:
+
++	Addition
+-	Subtraction
+*	Multiplication
+/	Division
+%	Modulus
+**	Exponentiation
+
+------------------------------
+
+PHP Assignment Operators:
+
+Assignment || Same as
+
+x = y  || x = y
+x += y || x = x + y
+x -= y || x = x - y
+x *= y || x = x * y
+x /= y || x = x / y
+x %= y || x = x % y
+
+------------------------------
+
+PHP Comparison Operators:
+
+Operator	    Name	              Example
+
+==              Equal	              $x == $y
+===	          Identical               $x === $y
+!=            Not equal               $x != $y
+<>            Not equal               $x <> $y
+!==         Not identical             $x !== $y
+>            Greater than             $x > $y
+<             Less than               $x < $y
+>=      Greater than or equal to	  $x >= $y
+<=      Less than or equal to         $x <= $y
+<=>           Spaceship	              $x <=> $y
+
+------------------------------
+
+PHP Increment / Decrement Operators:
+
+Operator	Name	
+++$x	Pre-increment
+$x++	Post-increment
+--$x	Pre-decrement
+$x--	Post-decrement
+
+------------------------------
+
+// PHP Logical Operators:
+
+Operator	Name	Example
+
+and	        And	    $x and $y	
+or	        Or	    $x or $y	
+xor	        Xor	    $x xor $y	
+&&	        And	    $x && $y	
+||	        Or	    $x || $y
+!	        Not	    !$x
+
+------------------------------
+
+// PHP String Operators:
+
+
+
+Operator	    Name	                Example	
+
+.	        Concatenation	        $txt1 . $txt2
+.=	       Concatenation        assignment	$txt1 .= $txt2
+
+------------------------------
+
+// PHP Array Operators
+
+Operator    	Name    	Example
+
++	           Union	    $x + $y	
+==	          Equality	    $x == $y
+===	          Identity	    $x === $y
+!=	         Inequality	    $x != $y
+<>           Inequality	    $x <> $y
+!==	        Non-identity	$x !== $y
+
+------------------------------
+
+// PHP Conditional Assignment Operators:
+
+Operator	    Name    	              Example	
+
+?:	          Ternary	           $x = expr1 ? expr2 : expr3
+??	       Null coalescing	       $x = expr1 ?? expr2
+
+------------------------------
+
+*/
+
