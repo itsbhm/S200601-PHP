c0e9064178e98ef404831139638c4900a35a7c6b
Shubham <shubham@itsbhm.group>
2021-05-18 12:18:46 +0530

    Added Number Topic #2

---
+++ 5_Numbers/home.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 10.10;
+
+// Check given value type is INT or not
+// var_dump( is_int($a) );
+
+// Check given value type is Float or not
+// var_dump( is_float($a) );
+
+
+// Check given value type is is_numeric
+var_dump( is_numeric($a) );
